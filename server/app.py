0a77bcfcab6245409824693ee43535e19b06b43e
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-22 12:51:36 +0300

    models added

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import db,User,Restaurant,Table,Booking,Review
 
 from views import *
 
