17a9a9be4cc8231112daac4c1e8977964bc92e9a
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-23 14:08:29 +0300

    edited

--- server/views/user_view.py
+++ server/views/user_view.py
@@ -3,8 +3,7 @@ from flask import request, jsonify, Blueprint
 from werkzeug.security import generate_password_hash
 from flask_jwt_extended import  jwt_required, get_jwt_identity
 
-
-
+user_bp = Blueprint('user_bp', __name__)
 
 #add users
 @user_bp.route("/users", methods=["POST"])
