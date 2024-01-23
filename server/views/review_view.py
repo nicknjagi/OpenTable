194512d559d21600c165475bf442d1dfa9004235
agent19music <seanmotanya@gmail.com>
2024-01-23 14:46:02 +0300

    update review view

--- server/views/review_view.py
+++ server/views/review_view.py
@@ -14,3 +14,12 @@ def get_review(id):
         return jsonify(review.to_dict()), 200
     else:
         return jsonify({"error": "Review not found"}), 404
+    
+@review_bp.route('/reviews/<int:id>',methods=['DELETE'])
+def delete_review(id):
+    review = db.session.query(Review).filter_by(id=id).one_or_none()
+    if review is None:
+        return jsonify({"error":"No such review exists"}), 404
+    db.session.delete(review)
+    db.session.commit()
+    return jsonify("Successfully deleted the review"), 200
