106bdd852b787d0f9ae65386ec10d3685ae122d1
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-23 15:16:43 +0300

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Layout from './layouts/Layout'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Restaurants from './pages/Restaurants'
+import RestaurantDetail from './pages/RestaurantDetail'
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/restaurants" element={<Restaurants />} />
+        <Route path="/restaurant/:id" element={<RestaurantDetail />} />
       </Route>
     </Routes>
   )
