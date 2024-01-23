825f8c471f03193e8eff32746a7504c6258c4ed6
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-23 14:13:51 +0300

    default

--- server/views/user_view.py
+++ server/views/user_view.py
@@ -1,1 +1,31 @@
+from models import db, User
+from flask import request, jsonify, Blueprint
+from werkzeug.security import generate_password_hash
+from flask_jwt_extended import  jwt_required, get_jwt_identity
 
+user_bp = Blueprint('user_bp', __name__)
+
+#add users
+@user_bp.route("/users", methods=["POST"])
+def add_users():
+    pass
+
+#fetch all users
+@user_bp.route("/users")
+def get_users():
+    pass
+
+#fetch single user
+@user_bp.route('/users/<int:id>')
+def get_user(user_id):
+    pass
+
+#update user
+@user_bp.route("/users/edit", methods=['PUT'])
+def update_user():
+    pass
+
+#delete user
+@user_bp.route("/users", methods=["DELETE"])
+def delete_user():
+    pass
