5ed0f44bbfd7416c283507ac69942ecdd0ec9253
agent19music <seanmotanya@gmail.com>
2024-01-23 14:35:12 +0300

    update review view

--- server/views/review_view.py
+++ server/views/review_view.py
@@ -2,3 +2,7 @@ from models import db, Review
 from flask import request, jsonify, Blueprint
 
 review_bp = Blueprint('review_bp', __name__)
+@review_bp.route('/reviews',methods=['GET'])
+def get_reviews():
+    reviews = [review.to_dict() for review in db.session.query(Review).all()]
+    return jsonify(reviews), 200
