de6835c5e7611e8943e73bfeb758ba7557b8d014
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 22:33:24 +0300

    update AddReviewForm.js

--- client/src/components/AddReviewForm.js
+++ client/src/components/AddReviewForm.js
@@ -1,29 +1,57 @@
 import React from 'react'
 import { Button, Textarea, Label, TextInput } from 'flowbite-react'
+import { useParams } from 'react-router-dom'
 
-const AddReviewForm = () => {
+const AddReviewForm = ({onchange, setOnchange}) => {
+    const {id} = useParams()
+
+    const authToken = sessionStorage.getItem('authToken')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const comment = formData.get('comment')
+        const rating = formData.get('rating')
+        if(!comment || !rating) return
+        fetch('/reviews',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${authToken && authToken}`,
+            },
+            body: JSON.stringify({comment, rating, restaurant_id:id})
+        })
+        .then(res => {
+            if(res.ok){
+                setOnchange(!onchange)
+            }
+        })
+    }
   return (
-    <form className="flex max-w-md flex-col gap-4 mt-4 mx-auto">
-      <div className="max-w-md">
-        <div className="mb-2 block">
-          <Label htmlFor="review" value="Add review" />
-        </div>
-        <Textarea id="review" name="review" required rows={4} />
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-4 w-full">
+      <div className="flex flex-col md:flex-row gap-4">
+          <div className='w-full'>
+            <div className="mb-2 block">
+              <Label htmlFor="comment" value="Comment" />
+            </div>
+            <Textarea id="comment" name="comment" required rows={3} />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="rating" value="Rating" />
+            </div>
+            <TextInput
+              className="md:w-24"
+              id="rating"
+              name="rating"
+              type="number"
+              min="1"
+              max="5"
+              required
+            />
+          </div>
       </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="rating" value="Rating" />
-        </div>
-        <TextInput
-          id="rating"
-          name="rating"
-          type="number"
-          min="1"
-          max="5"
-          required
-        />
-      </div>
-      <Button type="submit" className="w-1/2">
+      <Button gradientDuoTone="cyanToBlue" type="submit" className="md:w-1/3">
         Add review
       </Button>
     </form>
