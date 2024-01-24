357f0d7c8388a4929c519aa14b85cad2b936f03b
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 19:26:04 +0300

    update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,10 @@
 import { Navbar, Button } from 'flowbite-react'
+import { useContext } from 'react'
 import {NavLink} from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
+
 export default function Nav() {
+    const {currentUser, logout} = useContext(UserContext)
   return (
     <Navbar fluid className="max-w-[1280px] px-0 mx-auto">
       <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
@@ -9,15 +13,49 @@ export default function Nav() {
       <Navbar.Toggle />
       <Navbar.Collapse>
         <div className="flex px-4 md:px-0 flex-col md:flex-row gap-4 items-end md:items-center">
-          <NavLink className={({isActive}) => isActive ? "text-cyan-500" : null} to="/">Home</NavLink>
-          <NavLink className={({isActive}) => isActive ? "text-cyan-500" : null} to='/restaurants'>Restaurants</NavLink>
-          <NavLink className={({isActive}) => isActive ? "text-cyan-500" : null} to='/reservations'>Reservations</NavLink>
-          <NavLink className={({isActive}) => isActive ? "text-cyan-500" : null} to="/login">Login</NavLink>
-          <NavLink className={({isActive}) => isActive ? "text-cyan-500" : null} to="/signup">
-            <Button gradientDuoTone="cyanToBlue" size="xs">
-              Sign Up
-            </Button>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'text-cyan-500' : null)}
+            to="/">
+            Home
           </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'text-cyan-500' : null)}
+            to="/restaurants">
+            Restaurants
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'text-cyan-500' : null)}
+            to="/reservations">
+            Reservations
+          </NavLink>
+          {currentUser ? (
+            <NavLink
+              className={({ isActive }) => (isActive ? 'text-cyan-500' : null)}
+              to="/signup">
+              <Button onClick={logout} gradientDuoTone="cyanToBlue" size="xs">
+                Logout
+              </Button>
+            </NavLink>
+          ) : (
+            <>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'text-cyan-500' : null
+                }
+                to="/login">
+                Login
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'text-cyan-500' : null
+                }
+                to="/signup">
+                <Button gradientDuoTone="cyanToBlue" size="xs">
+                  Sign Up
+                </Button>
+              </NavLink>
+            </>
+          )}
         </div>
       </Navbar.Collapse>
     </Navbar>
