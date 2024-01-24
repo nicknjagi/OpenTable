258b78f02bb07cff8c640dd763df692ebf9500ec
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 14:04:22 +0300

    update Restaurants.js

--- client/src/pages/Restaurants.js
+++ client/src/pages/Restaurants.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import RestaurantCard from '../components/RestaurantCard'
+import { RestaurantsContext } from '../context/RestaurantsContext'
 
 const Restaurants = () => {
+    const {restaurants} = useContext(RestaurantsContext)
+
   return (
     <main className="px-6">
       <h1 className="text-2xl md:text-4xl text-center my-12">
         Book a reservation at a restaurant of your choosing.
       </h1>
       <section className="w-full justify-center mb-12 max-w-[1280px] mx-auto flex flex-wrap gap-x-4 gap-y-6">
-       {Array.from([1,2,3,4,5,6]).map((i) => {
-        return <RestaurantCard id={4} key={i} />
+       {restaurants.map((restaurant) => {
+        return <RestaurantCard restaurant={restaurant} key={restaurant.id} />
        })}
       </section>
     </main>
