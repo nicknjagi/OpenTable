001bc7ad532432d97ca734b9c34a329ca517ccaf
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 23:40:04 +0300

    update BookingForm.js

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button, Datepicker, Label, TextInput } from 'flowbite-react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
-const BookingForm = () => {
+const BookingForm = ({ onchange, setOnchange }) => {
+  const { authToken } = useContext(UserContext)
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const dataObj = Object.fromEntries(formData)
+    fetch('/bookings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken && authToken}`,
+      },
+      body: JSON.stringify({ ...dataObj, restaurant_id: id }),
+    }).then(res => {
+        setOnchange(!onchange)
+        navigate('/reservations')
+    })
+  }
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form onSubmit={handleSubmit} className="flex max-w-md flex-col gap-4">
       <h5 className="text-2xl font-semibold text-center">Make a reservation</h5>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="date" value="Pick a date" />
         </div>
-        <Datepicker id="date" />
+        <Datepicker name="booking_name" id="date" required />
       </div>
       <div>
         <div className="mb-2 block">
           <Label htmlFor="size" value="Party size" />
         </div>
-        <TextInput className="w-24" id="size" type="number" min='1' required />
+        <TextInput
+          className="w-24"
+          id="size"
+          name="party_size"
+          type="number"
+          min="1"
+          required
+        />
       </div>
       <div>
         <div className="mb-2 block">
@@ -27,7 +56,8 @@ const BookingForm = () => {
           className="rounded-lg border border-neutral-300 focus:border-transparent focus:outline-cyan-100"
           type="time"
           id="time"
-          name="time"
+          name="booking_time"
+          required
         />
       </div>
       <Button gradientDuoTone="cyanToBlue" type="submit">
