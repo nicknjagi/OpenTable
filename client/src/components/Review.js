1fbf05cae66d1f79a674a374840b69f04201872a
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 16:12:31 +0300

    update Review.js

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,32 +1,25 @@
 import React from 'react'
 import { Rating } from 'flowbite-react'
 
-const Review = () => {
+const Review = ({review}) => {
+  
   return (
     <li className="border border-zinc-300 py-2 px-4 rounded-md">
       <div className='flex gap-4 items-center my-2'>
         <img className='w-12 h-12 rounded-full object-cover object-top'
-          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src={review.user.profile_img}
           alt="profile pic"
         />
-        <span>Sasrah</span>
+        <span>{review.user.username}</span>
       </div>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
-        minima cumque quasi, inventore delectus ratione distinctio consectetur
-        porro itaque quidem?
+        {review.comment}
       </p>
       <div className="flex justify-between mt-2">
-        <span>
-          <Rating>
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star />
-            <Rating.Star filled={false} />
-          </Rating>
+        <span>Rating: {" "} 
+          {review.rating}
         </span>
-        <small>23/01/2024</small>
+        <small className='text-zinc-400'>{review.date_posted}</small>
       </div>
     </li>
   )
