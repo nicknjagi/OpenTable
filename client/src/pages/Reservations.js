8a88ee94fdb1e2174ea6efe99a47fe451e0add91
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-24 10:29:51 +0300

    feat: create Reservations.js

---
+++ client/src/pages/Reservations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Booking from '../components/Booking'
+
+const Reservations = () => {
+  return (
+    <ul className='my-12'>
+      <Booking />
+    </ul>
+  )
+}
+
+export default Reservations
