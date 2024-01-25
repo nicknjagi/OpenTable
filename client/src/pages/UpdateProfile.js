8a41a1d5e93f9b91a8fdac5e983e5cbbbbeafb48
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-25 20:24:56 +0300

    update UpdateProfile.js

--- client/src/pages/UpdateProfile.js
+++ client/src/pages/UpdateProfile.js
@@ -1,4 +1,4 @@
-import {React, useContext, useState} from 'react'
+import {React, useContext} from 'react'
 import { Button, Label, TextInput } from 'flowbite-react'
 import { UserContext } from '../context/UserContext';
 import Swal from "sweetalert2"
