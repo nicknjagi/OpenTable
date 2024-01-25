2b222fe062386fbd2889ac9b9ced9e42182da692
Nicholas Njagi <njaginicholas@gmail.com>
2024-01-25 14:28:33 +0300

    update UpdateProfile.js

--- client/src/pages/UpdateProfile.js
+++ client/src/pages/UpdateProfile.js
@@ -2,9 +2,10 @@ import {React, useContext, useState, useEffect} from 'react'
 import { Button, Label, TextInput } from 'flowbite-react'
 import { UserContext } from '../context/UserContext';
 import Swal from "sweetalert2"
+import { useNavigate } from 'react-router-dom';
 
 export default function UpdateProfile() {
-const { currentUser, setCurrentUser } = useContext(UserContext);
+const { currentUser, authToken, setOnchange, onchange } = useContext(UserContext);
 
 const [user, setUser] = useState({
   username: currentUser ? currentUser.username : '',
@@ -14,6 +15,7 @@ const [user, setUser] = useState({
   first_name: currentUser ? currentUser.first_name : '',
   last_name: currentUser ? currentUser.last_name : ''
 });
+const navigate = useNavigate()
 
 const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,95 +34,107 @@ const handleSubmit = (event) => {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${currentUser.token}`,
+        Authorization: `Bearer ${authToken}`,
       },
       body: JSON.stringify(user),
     })
     .then(response => {
       if (response.ok) {
+        setOnchange(!onchange)
+        Swal.fire('Success', 'Profile updated successfully', 'success')
+        navigate('/profile')
         return response.json();
-      } else {
-        throw new Error('Something went wrong');
-      }
-    })
-    .then(data => {
-      setCurrentUser({
-        ...currentUser,
-        ...data,
-      });
-      Swal.fire("Success", "Profile updated successfully", "success");
+      } 
     })
     .catch(error => {
       Swal.fire("Error", error.message, "error");
     });
+    console.log(user);
   };
-console.log(JSON.stringify(user));
   return (
     <div>
-        <form onSubmit={handleSubmit} className="flex max-w-md mx-auto mt-24 px-4 flex-col gap-4">
-      <h3 className="text-2xl text-center">Update Profile</h3>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="username" value="Username" />
+      <form
+        onSubmit={handleSubmit}
+        className="flex max-w-md mx-auto my-24 px-4 flex-col gap-4 md:px-6 border py-10 rounded-2xl border-zinc-300 shadow-xl">
+        <h3 className="text-2xl text-center">Update Profile</h3>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="username" value="Username" />
+          </div>
+          <TextInput
+            id="username"
+            name="username"
+            type="text"
+            required
+            defaultValue={currentUser ? currentUser.username : ''}
+            placeholder="Type username here"
+          />
         </div>
-        <TextInput
-          id="username"
-          name='username'
-          type="text"
-          required
-          defaultValue={currentUser ? currentUser.username : ''}
-          placeholder='Type username here'
-        />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email" value="Email" />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email" value="Email" />
+          </div>
+          <TextInput
+            id="email"
+            name="email"
+            type="text"
+            defaultValue={currentUser ? currentUser.email : ''}
+            placeholder="Type email here"
+            required
+          />
         </div>
-        <TextInput id="email" name='email' type="text" 
-        defaultValue={currentUser ? currentUser.email : ''}
-        placeholder='Type email here'
-        required />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="firstname" value="First Name" />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="firstname" value="First Name" />
+          </div>
+          <TextInput
+            id="firstname"
+            name="firstname"
+            type="text"
+            defaultValue={currentUser ? currentUser.first_name : ''}
+            placeholder="Type first name here"
+          />
         </div>
-        <TextInput id="firstname" name='firstname' type="text" 
-        defaultValue={currentUser ? currentUser.first_name : ''}
-        placeholder='Type first name here'
-         />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="lastname" value="Last Name" />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="lastname" value="Last Name" />
+          </div>
+          <TextInput
+            id="lastname"
+            name="lastname"
+            type="text"
+            defaultValue={currentUser ? currentUser.last_name : ''}
+            placeholder="Type last name here"
+          />
         </div>
-        <TextInput id="lastname" name='lastname' type="text" 
-        defaultValue={currentUser ? currentUser.last_name : ''}
-        placeholder='Type last name here'
-         />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="phone" value="Phone" />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="phone" value="Phone" />
+          </div>
+          <TextInput
+            id="phone"
+            name="phone"
+            type="text"
+            defaultValue={currentUser ? currentUser.contact_info : ''}
+            placeholder="Type phone number here"
+          />
         </div>
-        <TextInput id="phone" name='phone' type="text" 
-        defaultValue={currentUser ? currentUser.contact_info : ''}
-        placeholder='Type phone number here'
-         />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="profilepicture" value="Profile picture" />
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="profilepicture" value="Profile picture" />
+          </div>
+          <TextInput
+            id="profilepicture"
+            name="profilepicture"
+            type="url"
+            defaultValue={currentUser ? currentUser.profile_img : ''}
+            placeholder="Paste image link here"
+          />
         </div>
-        <TextInput id="profilepicture" name='profilepicture' type="url" 
-        defaultValue={currentUser ? currentUser.profile_img : ''}
-        placeholder='Paste image link here'
-         />
-      </div>
-      <Button gradientDuoTone="cyanToBlue" type="submit">
-        Update
-      </Button>
-    </form>
+        <Button gradientDuoTone="cyanToBlue" type="submit">
+          Update
+        </Button>
+      </form>
     </div>
   )
 }
