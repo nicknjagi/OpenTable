f5d1f4f22a08eda64dfbe475a8e3908a3183c0fc
Nick <njaginicholas@gmail.com>
2024-12-05 08:55:48 +0000

    chore: rename App.js to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
 import Layout from './layouts/Layout'
 import Home from './pages/Home'
 import Login from './pages/Login'
