d754035678b2b95668fd9565aa356d96f3cf8b6c
Nick <njaginicholas@gmail.com>
2024-12-07 11:39:14 +0300

    update seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
-from models import db,User,Restaurant, Booking,Review
+from models import db, User, Restaurant, Booking, Review
 from random import sample, randint
 from faker import Faker
 from sqlalchemy import func
 from werkzeug.security import generate_password_hash
 from app import app
 
-fake=Faker()
+fake = Faker()
+
 
 def seed_user():
     users = []
@@ -18,24 +19,41 @@ def seed_user():
             profile_img=fake.image_url(),
             contact_info=fake.phone_number(),
             first_name=fake.first_name(),
-            last_name=fake.last_name()
+            last_name=fake.last_name(),
         )
         users.append(user)
     db.session.add_all(users)
     db.session.commit()
 
+
 def seed_restaurant():
     restaurants = []
+    restaurantImages = [
+        "https://images.pexels.com/photos/2287523/pexels-photo-2287523.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/3201921/pexels-photo-3201921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/3534750/pexels-photo-3534750.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "https://images.pexels.com/photos/1581384/pexels-photo-1581384.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/262047/pexels-photo-262047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/914388/pexels-photo-914388.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/1449773/pexels-photo-1449773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://images.pexels.com/photos/1741285/pexels-photo-1741285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/8753546/pexels-photo-8753546.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+        "https://images.pexels.com/photos/601169/pexels-photo-601169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ]
+    count = 0
     for _ in range(12):
         restaurant = Restaurant(
             name=fake.company(),
-            phone_no=int(("").join(fake.msisdn().replace('-','')[:9])),
+            phone_no=int(("").join(fake.msisdn().replace("-", "")[:9])),
             description=fake.text(),
-            restaurant_img=fake.image_url(),
+            restaurant_img=restaurantImages[count],
             location=fake.address(),
             capacity=randint(20, 100),
-            owner_id=User.query.order_by(func.random()).first().id
+            owner_id=User.query.order_by(func.random()).first().id,
         )
+        count += 1
         restaurants.append(restaurant)
     db.session.add_all(restaurants)
     db.session.commit()
@@ -48,13 +66,14 @@ def seed_booking():
             user_id=User.query.order_by(func.random()).first().id,
             restaurant_id=Restaurant.query.order_by(func.random()).first().id,
             booking_date=datetime.utcnow() + timedelta(days=randint(1, 30)),
-            booking_time=datetime.strptime(fake.time(), '%H:%M:%S').time(),
+            booking_time=datetime.strptime(fake.time(), "%H:%M:%S").time(),
             party_size=randint(1, 10),
-            status=sample(['confirmed', 'pending', 'cancelled'], 1)[0]
+            status=sample(["confirmed", "pending", "cancelled"], 1)[0],
         )
         bookings.append(booking)
     db.session.add_all(bookings)
     db.session.commit()
+
 
 def seed_review():
     reviews = []
@@ -66,13 +85,14 @@ def seed_review():
                 user_id=User.query.order_by(func.random()).first().id,
                 rating=randint(1, 5),
                 comment=fake.text(),
-                date_posted=datetime.utcnow() + timedelta(days=randint(1, 365))
+                date_posted=datetime.utcnow() + timedelta(days=randint(1, 365)),
             )
             reviews.append(review)
     db.session.add_all(reviews)
     db.session.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     with app.app_context():
         db.session.query(User).delete()
         db.session.query(Restaurant).delete()
